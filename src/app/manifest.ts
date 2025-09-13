59034e542896cf1ca01821be326a0cecf1002771
darka1pha <abolfazl.omrani1999@gmail.com>
2025-09-13 15:18:44 +0330

    manifest files added

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,16 +9,31 @@ export default function manifest(): MetadataRoute.Manifest {
     scope: '.',
     display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#000000',
+    theme_color: '#ffffff',
     icons: [
       {
-        src: '/icon-192x192.png',
+        src: '/assets/manifest-icons/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+      {
+        src: '/assets/manifest-icons/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/icon-512x512.png',
+        src: '/assets/manifest-icons/android-chrome-512x512.png',
         sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/assets/manifest-icons/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        src: '/assets/manifest-icons/favicon-32x32.png',
+        sizes: '32x32',
         type: 'image/png',
       },
     ],
